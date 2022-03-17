490bba40e5127c8cba2ed723332dab2e5386e099
@devCluna <jobs.cluna@gmail.com>
2022-03-17 18:02:50 -0500

    💄 feat(sidebar): sidebar comp & repos holder @devCluna

---
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,139 @@
+import React from 'react'
+import './Sidebar.css'
+import {BiBookBookmark} from 'react-icons/bi'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <p>Repositories</p>
+        <div className='repo-holder'>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+        <div className='repo-item'>
+            <BiBookBookmark />
+            <div>dlas</div>
+        </div>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar

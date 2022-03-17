edcd353de952eefb8bdf54b88054c5f6e70468a0
@devCluna <jobs.cluna@gmail.com>
2022-03-17 12:41:13 -0500

    💄 feat(app): add home comp to app @devCluna

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     
+     <Home/>
 
     </div>
   )

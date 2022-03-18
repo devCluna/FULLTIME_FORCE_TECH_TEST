aa4ee36c403f438d8616d5fd3f40184ce827eb11
@devCluna <jobs.cluna@gmail.com>
2022-03-17 20:07:38 -0500

    💄 feat(body): add repos comp to body @devCluna

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Repos from '../Repos/Repos'
 import Sidebar from '../Sidebar/Sidebar'
 import './Body.css'
+
 const Body = () => {
   return (
     <div className='body'>
        <Sidebar/>
+       <Repos/>
     </div>
   )
 }

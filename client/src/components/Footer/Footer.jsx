d135e023dee9fe1505be539d6cf50e7fe4fd615d
@devCluna <jobs.cluna@gmail.com>
2022-03-18 02:45:37 -0500

    💄 feat(footer): add links to social media @devCluna

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -13,24 +13,38 @@ const Footer = () => {
     <div className='support-holder'>
       <div className='support-tag'>Supported by</div>
       <div className='supporter'>
-        <DiGithubFull className='logo1'/>
+        <DiGithubFull className='logo1' onClick={()=>{
+          window.open("https://github.com",'_blank')
+        }}/>
       </div>
     </div>
 
     <div className='support-holder'>
       <div className='support-tag'>Developed by</div>
-      <div className='supporter'>
-        @devCluna
+      <div className='supporter' onClick={()=>{
+          window.open("https://devCluna.com",'_blank')
+        }}>
+          <a href='https://devCluna.com'>
+          @devCluna
+          </a>
       </div>
     </div>
 
     <div className='support-holder'>
       <div className='support-tag'>Social Media</div>
       <div className='supporter'>
-        <BsGithub/>
-        <BiWorld/>
-        <BsLinkedin/>
-        <BsTwitter/>
+        <BsGithub className='supporter-icon' onClick={()=>{
+          window.open("https://github.com/devCluna",'_blank')
+        }}/>
+        <BiWorld className='supporter-icon' onClick={()=>{
+          window.open("https://devcluna.com",'_blank')
+        }}/>
+        <BsLinkedin className='supporter-icon' onClick={()=>{
+          window.open("https://www.linkedin.com/in/devcluna",'_blank')
+        }}/>
+        <BsTwitter className='supporter-icon' onClick={()=>{
+          window.open("https://twitter.com/DevCLuna",'_blank')
+        }}/>
       </div>
       
     </div>

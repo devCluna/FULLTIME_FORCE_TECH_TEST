f0d5a15871dfe191a068794df47e291beb164a00
@devCluna <jobs.cluna@gmail.com>
2022-03-18 00:51:55 -0500

    🍱 reafactor(sidebar) update repos src @devCluna

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,136 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Sidebar.css'
 import {BiBookBookmark} from 'react-icons/bi'
+import axios from 'axios'
 
 const Sidebar = () => {
+    
+    const [repos, setRepos] = useState([])
+    useEffect(()=>{
+        axios.get("https://api.github.com/users/devCluna/repos")
+            .then(item =>{
+                setRepos(item.data)
+
+            })
+    },[])
+
   return (
     <div className='sidebar'>
         <p>Repositories</p>
         <div className='repo-holder'>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-        <div className='repo-item'>
-            <BiBookBookmark />
-            <div>dlas</div>
-        </div>
-
-
+            {repos?.map(repo => (
+                <div key={repo.id} className='repo-item'>
+                    <div className='repo-icon'>
+                        <BiBookBookmark />
+                    </div>
+                    <div className='repo-name'>{repo.name}</div>
+            </div>
+            ))}
         </div>
     </div>
   )

e5becac5aa0d9493fa85b74aa1d5157503ac3351
@devCluna <jobs.cluna@gmail.com>
2022-03-20 22:01:19 -0500

    🔥chore(redux): remove listrepo & selectedRepo @devCluna

--- client/src/redux/selectedRepo/selectedRepoActions.js
+++
@@ -1,8 +0,0 @@
-import { PICK_REPO } from "./selectedRepoTypes";
-
-export const pickRepo = (repo) => {
-    return {
-        type: PICK_REPO,
-        payload: repo
-    }
-}
--- client/src/redux/selectedRepo/selectedRepoTypes.js
+++
@@ -1,1 +0,0 @@
-export const PICK_REPO = 'PICK_REPO'

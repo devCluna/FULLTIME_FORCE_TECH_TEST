ee7aed7b638e806d9fe20059170d8afc50c7e21b
@devCluna <jobs.cluna@gmail.com>
2022-03-20 22:24:07 -0500

    ♻️ ref(store): add userInfo & nav reducers @devCluna

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,12 @@
 import { composeWithDevTools } from "redux-devtools-extension";
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
-import listReposReducer from "./listRepo/listRepoReducer";
-import selectedRepoReducer from "./selectedRepo/selectedRepoReducer";
+import userInfoReducer from "./userInfo/userInfoReducer";
+import navReducer from "./navigation/navReducer";
 
 const  rootReducer = combineReducers({
-   listRepos: listReposReducer,
-   selectedRepo: selectedRepoReducer
+   userInfo: userInfoReducer,
+   nav: navReducer
 })
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))

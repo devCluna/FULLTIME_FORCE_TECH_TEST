26a6d200c3569d8099b16991efa4f4340ff20c5e
@devCluna <jobs.cluna@gmail.com>
2022-03-20 22:29:43 -0500

    feat(repos): add pickedRepo & commits display @devCluna

--- client/src/components/Repos/Repos.jsx
+++ client/src/components/Repos/Repos.jsx
@@ -2,16 +2,16 @@ import React, { useEffect, useRef, useState } from 'react'
 import './Repos.css'
 import {TiArrowSortedUp} from 'react-icons/ti'
 import {TiArrowSortedDown} from 'react-icons/ti'
-import axios from 'axios'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectBranch } from '../../redux/userInfo/userInfoActions'
 
 const Repos = () => {
-  const selectedRepo = useSelector(state => state.selectedRepo.selectedRepo)
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.userInfo.user)
 
   const selectorRef = useRef()
   const [toggleBranch, setToggleBranch] = useState(false)
-  const [branches, setBranches] = useState([])
-  const [commits, setCommits] = useState([])
+  const [currentBranch, setCurrentBranch] = useState("master")
 
   const getHours = (postDate, currentDate) => {
     const milliseconds = Math.abs(currentDate - new Date(postDate))
@@ -35,18 +35,6 @@ const Repos = () => {
    return newDate
   }
 
-  useEffect(()=>{
-
-    axios.get('https://api.github.com/repos/devcluna/FULLTIME_FORCE_TECH_TEST/branches')
-    .then(item =>{
-      setBranches(item.data)
-      // console.log(item.data[item.data.length-1].commit.url)
-      axios.get('https://api.github.com/repos/devcluna/FULLTIME_FORCE_TECH_TEST/commits?sha=d6fc5ba1cc13450e0785ffbdd4db1871fea4cdfd&per_page=100')
-        .then(res =>{
-          setCommits(res.data)
-        })
-    })
-  },[])
 
   useEffect(()=>{
     const closeSelector= (e) => {
@@ -62,38 +50,43 @@ const Repos = () => {
     <div className="repos">
       <h3 className="section-repos-title">Selected Repo</h3>
       <div className="repos-item">
-        <a className="repo-title" href={selectedRepo.html_url} target="_blank">{selectedRepo.full_name}</a>
-        <span className="repo-description">{selectedRepo.description ? selectedRepo.description :'No description provided' }</span>
-        <span className='repo-date-label'>created at: <span className='repo-date'>{selectedRepo.created_at && getDate(new Date(selectedRepo.created_at))}</span></span>
-        <span className='repo-date-label'>updated at: <span className='repo-date'>{selectedRepo.updated_at && getDate(new Date(selectedRepo.updated_at))}</span></span>
+        <a className="repo-title" href={user?.selectedRepo?.html_url} target="_blank">{user?.selectedRepo?.full_name}</a>
+        <span className="repo-description">{user?.selectedRepo?.description ? user?.selectedRepo?.description :'No description provided' }</span>
+        <span className='repo-date-label'>created at: <span className='repo-date'>{user?.selectedRepo?.created_at && getDate(new Date(user.selectedRepo?.created_at))}</span></span>
+        <span className='repo-date-label'>updated at: <span className='repo-date'>{user?.selectedRepo?.updated_at && getDate(new Date(user.selectedRepo?.updated_at))}</span></span>
         <div className="techs-stacks">
-          {selectedRepo.topics?.map(topic=>(
+          {user?.selectedRepo?.topics?.map(topic=>(
             <span>{topic}</span>
           ))}
         </div>
       </div>
 
-      <h3 className="section-repos-title">Selected Repo</h3>
+      <h3 className="section-repos-title">Selected Branch</h3>
       <div className="branchs">
         <div ref={selectorRef} className="branch-selector" onClick={() => setToggleBranch(!toggleBranch)}>
-          <div>master</div>
+          <div>{user?.selectedBranch?.name ? user?.selectedBranch?.name : currentBranch}</div>
           {toggleBranch ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
         </div>
         {toggleBranch && (
           <div className="branch-options">
-            {branches?.map(branch => (
-              <div>{branch.name}</div>
+            {user?.branches?.map(branch => (
+              <div key={branch.commit.sha}
+                onClick={()=>{
+                  setCurrentBranch(branch.name)
+                  dispatch(selectBranch(user.user.login, user.selectedRepo.name, branch.commit.sha))
+                }}
+              >{branch.name}</div>
             ))}
           </div>
         )}
       </div>
 
       <div className="commits">
-        {commits?.map(commit =>(
+        {user?.commits?.map(commit =>(
           <div key={commit.node_id} className="commit-item">
             <a className="commit-description" href={commit.html_url} target="_blank">{commit.commit.message}</a>
             <div className="commit-details">
-              <img className="commit-author" src={commit.author.avatar_url} />
+              <img className="commit-author" src={commit?.author?.avatar_url}/>
               <span className="commit-date">commited {getHours(commit.commit.committer.date, new Date)} ago</span>
             </div>
         </div>
